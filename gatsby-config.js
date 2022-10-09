9073632e8afc0a5ff242fe0dae048618da0a16f3
John McAulay <john.mcaulay@banff.ca>
2022-10-08 19:14:49 -0600

    add google anayltics

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,50 +7,61 @@ module.exports = {
     title: `Pursuit of Excellence Academic Tutoring`,
     siteUrl: `https://poetutoring.com`
   },
-  plugins: [{
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "GA-XXXXXX"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+        "G-C3H60HNRBR", // Google Analytics / GA
+        ],
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true
+        },
+      },
     },
-    __key: "images"
-  },
-  'gatsby-plugin-postcss',
-  {
-    resolve: `gatsby-source-contentful`,
-    options: {
-      spaceId: `5j4d4ve70rgp`,
-      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp", 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
     },
-  },
-  `gatsby-plugin-image`,
-  `gatsby-plugin-sharp`,
-  {
-    resolve: `gatsby-plugin-google-fonts`,
-    options: {
-      fonts: [
-        `Alkalami`,
-        `Lato\:400,700` // you can also specify font weights and styles
-      ],
-      display: 'block'
+    'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `5j4d4ve70rgp`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
     },
-  },
-  {
-    resolve: `gatsby-plugin-manifest`,
-    options: {
-      name: `Pursuit of Excellence AcademicAcademic Tutoring and Dissertation Mentorin`,
-      short_name: `Pursuit of Excellenceg`,
-      start_url: `/`,
-      background_color: `#000000`,
-      theme_color: `#000000`,
-      display: `standalone`,
-      icon: `src/images/icon.png`
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Alkalami`,
+          `Lato\:400,700` // you can also specify font weights and styles
+        ],
+        display: 'block'
+      },
     },
-  },
-  `gatsby-plugin-sitemap`]
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Pursuit of Excellence AcademicAcademic Tutoring and Dissertation Mentorin`,
+        short_name: `Pursuit of Excellenceg`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
+      },
+    },]
 };
